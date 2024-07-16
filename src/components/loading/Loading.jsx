70c63ae3f0b1e85e12d9cc884e22d46018292580
Santiago Pasaje <dspasaje89@gmail.com>
2024-07-16 12:47:30 -0500

    Se crea proyecto challenge aluraflix - ONE Alura LATAM

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,11 @@
+import "./Loading.css";
+
+const Loading = () => {
+  return (
+    <div className="loading-container">
+      <h2>Espera un momento...</h2>
+    </div>
+  );
+};
+
+export default Loading;
